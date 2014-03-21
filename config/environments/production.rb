03ea378b39bf2ec4c418f8151d13ce8907665e26
bilton <native_bilton@hotmail.com>
2014-03-22 10:48:41 +1100

    removed contents of public/asset folder so that assets are precompiled at production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-config.assets.precompile += %w( .svg .eot .woff .ttf)
+ config.assets.precompile += %w( .svg .eot .woff .ttf)
 end
